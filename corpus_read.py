9b23f430b819da9548831a1e2a1917d01537437d
yavilarobledo <143647435+yavilarobledo@users.noreply.github.com>
2024-03-06 16:27:25 -0500

    asdk

---
+++ corpus_read.py
@@ -0,0 +1,9 @@
+file_path='/content/corpus.txt'
+def read_corpus(file_path):
+    with open(file_path, 'r') as file:
+        corpus_text = file.read()
+    return corpus_text
+
+def count_word_occurrences(corpus, token):
+    words = corpus.split()
+    return words.count(token)
